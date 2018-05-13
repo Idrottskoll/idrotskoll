98005683b8757a1473fa82451c292fcb983e7422
Jeremy Danner <jeremydanner2@gmail.com>
2018-05-13 13:45:57 +0200

    added router

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
-import { Faq } from './components/middleware/text';
-import {
-    Button,
-    Label,
-    Input,
-    TextArea,
-    Title,
-    Paragraph,
-    Link,
-    SmallTitle
-} from './components/styles';
-import StreamScheduleContainer from './components/streamSchedule/StreamScheduleContainer';
-import Footer from './components/layout/Footer';
-import Header from './components/layout/Header';
+import { Faq } from './middleware/text';
+import { Button, Label, Input, TextArea, Title, Paragraph, Link, SmallTitle } from './styles';
+import StreamScheduleContainer from './streamSchedule/StreamScheduleContainer';
+import Footer from './layout/Footer';
+import Header from './layout/Header';
 
 export default class App extends Component {
     render() {
