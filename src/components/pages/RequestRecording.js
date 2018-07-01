b4d10636ca935bad467359b8d803a3451d12f73f
Erica Glimsholt <erica@simmalugnt.se>
2018-07-01 15:55:47 +0200

    Updated markup and styling for RequestRecording component

--- src/components/pages/RequestRecording.js
+++ src/components/pages/RequestRecording.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Label, Input, TextArea, Button, Title, Paragraph, Link } from '../styles';
 import Footer from '../layout/Footer';
 import Header from '../layout/Header';
 
@@ -7,9 +8,35 @@ export default class RequestRecording extends Component {
         return (
             <div>
                 <Header />
-                <div className="row">
-                    <div className="col-xs-12">
-                        <h1>Inspelnings förfrågan</h1>
+                <div className="row tm-40">
+                    <div className="large-12">
+                        <Title>Lägg inspelningsförfrågan</Title>
+                    </div>
+                    <div className="large-6 rp-20">
+                      <Label>Ämne*</Label>
+                      <Input type="text" placeholder="Ex. installera kameror, livestreaming" />
+                      <Label>Namn*</Label>
+                      {/* error = add class "error" to input */}
+                      <Input type="text" placeholder="Ditt för- och efternamn" className="error" />
+                      <Label>Mail*</Label>
+                      <Input type="text" placeholder="Din mailadress" />
+                      <Label>Din klubb</Label>
+                      <Input type="text" placeholder="Namn på klubben det handlar om" />
+                    </div>
+                    <div className="large-6">
+                    <Label>Meddelande*</Label>
+                    <TextArea rows="3" columns="3" placeholder="Ditt meddelande..." className="bm-20" />
+                    <div className="inline bm-20">
+                      <div className="check rm-20">
+                        <Input id="checkbox" type="checkbox" />
+                      </div>
+                      <Label>Jag godkänner att Idrottskoll håller mina uppgifter enligt deras <Link>Privacy Policies.</Link></Label>
+                    </div>
+                    <Button>Skicka meddelande</Button>
+                    {/* error = show this message below */}
+                    <Paragraph>Uppgifterna stämde inte. Dubbelkolla alla fält med * är ifyllda och försök igen.</Paragraph>
+                    {/* success = show this message below and reset all inputs */}
+                    {/* <Paragraph>Tack för ditt meddelande! Vi hör av oss så fort vi kan.</Paragraph> */}
                     </div>
                 </div>
                 <Footer />
